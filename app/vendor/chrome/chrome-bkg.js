ee43ebaa7153db80a69f8c0f9378657ea9f94114
Arnaud Kervern <arnaud.k@gmail.com>
2017-07-14 14:57:02 -0400

    BDE-61: Add Working example with PhantomJS + sinon

--- app/vendor/chrome/chrome-bkg.js
+++ app/vendor/chrome/chrome-bkg.js
@@ -30,7 +30,7 @@ function onChange(tabInfo) {
   chrome.tabs.query({lastFocusedWindow: true, active: true}, getInfoForTab);
 };
 
-var target = "<all_urls>";
+var target = '<all_urls>';
 chrome.webRequest.onCompleted.addListener(onChange, {urls: [target]});
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   chrome.tabs.get(activeInfo.tabId, pageActionOnNuxeo);
